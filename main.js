a9057afb3acdb97a7e69a1d7cf3032e3bde27d64
Luan Nguyen <luan@elife.clinic>
2024-10-08 17:02:57 +0700

    remove console.log

--- main.js
+++ main.js
@@ -21,9 +21,6 @@ async function checkWeather(city) {
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-  console.log("Weather: ", data.weather[0].main);
-
-  //update
   switch (data.weather[0].main) {
     case "Clouds":
       weatherIcon.src = "images/clouds.png";
